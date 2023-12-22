f5c66616bf68c12b010c2a57d153d4a7e279f4ef
ronanter <ronan1terras@gmail.com>
2023-12-22 11:04:38 +0100

    [MERGE]

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -5,6 +6,22 @@ import java.io.IOException;
 public class Coordinator {
 
     String text = "";
+
+    public ArrayList<ArrayList<String>> splitList(ArrayList<String> listPhrases, int nbWorkers){
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+
+        int sublistSize = listPhrases.size() / nbWorkers;
+        int remainder = listPhrases.size() % nbWorkers;
+
+        int startIndex = 0;
+        for (int i = 0; i < nbWorkers; i++) {
+            int endIndex = startIndex + sublistSize + (i < remainder ? 1 : 0);
+            result.add(new ArrayList<>(listPhrases.subList(startIndex, endIndex)));
+            startIndex = endIndex;
+        }
+
+        return result;
+    }
 
     public String read(String path) {
         try (FileReader fileReader = new FileReader(path);
